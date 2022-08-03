bae22a194cc5d715467fa57797b562fd138c306d
Fabibourdokan <68827627+Fabibourdokan@users.noreply.github.com>
2022-08-03 19:06:45 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class beterraba extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Co0mponente de Classe</h1>
+      </div>
+    );
+  }
+}
